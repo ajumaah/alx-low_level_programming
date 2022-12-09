83d5d90fac4c62bc2502bcb89ebd456a93a7651e
Alfonse <jumaalfonse1@gmail.com>
2022-12-08 18:18:59 -0800

    A c program that uses put to print text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main entry point
+ *
+ * Return 0 as a succeess
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+
+	return (0);
+}
