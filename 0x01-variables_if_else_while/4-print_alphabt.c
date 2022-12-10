3296d62affa6ebe3ea579306313c2f345ea6e157
Alfonse <jumaalfonse1@gmail.com>
2022-12-10 05:10:14 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -17,7 +17,7 @@ int main(void)
 {
 	char alphabet;
 
-	while (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet != 'q' && alphabet != 'e')
 		putchar(alphabet);
