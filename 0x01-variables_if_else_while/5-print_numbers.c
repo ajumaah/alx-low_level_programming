d1ee08bb24fcc4419d426b49c2bb5f20d748381b
Alfonse <jumaalfonse1@gmail.com>
2022-12-10 05:23:36 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * Title: 5-print_numbers.c
+ * Auth: Alfonse Juma
+ * Date: Dec 10, 2022
+ * Time: 4:50 pm
+ */
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: prints single digits of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int 0;
+
+	while(i = 0; i <= 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	
+	return (0);
+}
