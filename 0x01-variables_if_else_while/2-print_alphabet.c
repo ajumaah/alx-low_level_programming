722452b9013d3005bd4029250c31a4157a400d80
Alfonse <jumaalfonse1@gmail.com>
2022-12-10 04:25:47 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,9 +17,8 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-		putchar(alphabet++);
-
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
 	putchar('\');
 	return (0);
 }
