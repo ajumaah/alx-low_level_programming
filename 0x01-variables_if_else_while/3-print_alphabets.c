24ee63843d41f3e9708ac87267ba8f13bb5dfc79
Alfonse <jumaalfonse1@gmail.com>
2022-12-10 04:43:40 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * Title: 3-print_alphabet.c
+ * Auth: Alfonse Juma
+ * Date: Dec 10, 2022
+ * Time: 3:30pm
+ */
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: A program that prints the alphabet in lowercase and uppercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)                                    putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
