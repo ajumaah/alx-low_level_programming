6431e541d73e14c6cf22130c40c51bf21211c523
Alfonse <jumaalfonse1@gmail.com>
2022-12-10 04:54:22 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,9 +17,8 @@ int main(void)
 {
 	char alphabet;
 
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
-	
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
 
