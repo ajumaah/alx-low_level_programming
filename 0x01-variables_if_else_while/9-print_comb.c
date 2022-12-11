3c091bf73d39f8fae5fd12e8af4e28dccb40c333
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 09:16:23 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,12 +18,18 @@ int main(void)
 	int num;
 
 	while (num < 10)
-		putchar((num % 10) + '0');
+	{
+		putchar((num % 10) + '0')
+			num++;
+		if (num == 10)
+			continue;
 	putchar(',');
-	
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	putchar(' ');
+	}
 	putchar('\n');
 	return (0);
+
+	
+
+	
 }
