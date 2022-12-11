de95c9eb57f4c45cd6fcb645fde6258a45cc2ed3
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 08:27:07 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/*
+ * Title: 8-print_base16.c
+ * Auth: Alfonse Juma
+ * Date: Dec 11, 2022
+ * Time: 7:13 pm
+ */
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: prints single digits of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+		putchar((num % 16) + '0');
+	putchar('\n');
+	return (0);
+}
