c339f7dc08a13d46bc0610b27dfcc1389bcfcf74
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 08:45:17 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -18,8 +18,10 @@ int main(void)
 	int num;
 	char alphabet;
 
-	for {(num = 0; num < 16; num++),(alphabet = 'a'; alphabet <= 'f'; alphabet++)}
-		putchar((num % 16) + '0', alphabet);
+	for (num = 0; num < 16; num++)
+		putchar((num % 16) + '0');
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+		putchar(alphabet);
 	putchar('\n');
 	return (0);
 }
