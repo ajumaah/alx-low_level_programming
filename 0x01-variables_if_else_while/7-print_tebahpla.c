c2a4f034b85919022aba453be951995a665aa62f
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 07:59:00 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/*
+ * Title:  7-print_tebahpla.c
+ * Auth: Alfonse Juma
+ * Date: Dec 11, 2022
+ * Time: 6:50 pm
+ */
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: prints lowercae in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet < 'a'; alphabet++)
+		putchar('alphabet');
+	putchar('\n');
+	return (0);
+}
