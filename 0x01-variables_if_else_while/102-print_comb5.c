58d4844d60eb9a7caeb3ec1c174589f4a4738253
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 11:50:57 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,10 @@ int main(void)
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar(' ');
 			putchar((num2 % 10) + '0');
+			putchar((num2 / 10) + '0');
 			if (num1 == 98 && num2 == 99)
 				continue;
 			putchar(',');
