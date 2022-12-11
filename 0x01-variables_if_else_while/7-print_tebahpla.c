97e0e93d4dc8b51d286c8ef85cca2f7afd176764
Alfonse <jumaalfonse1@gmail.com>
2022-12-11 08:06:03 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -17,7 +17,7 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'z'; alphabet => 'a'; alphabet++)
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
 	return (0);
