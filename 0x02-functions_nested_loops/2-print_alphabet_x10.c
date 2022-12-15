d9d7359b641a8f6d5be2435b7276576c953057e9
Alfonse <jumaalfonse1@gmail.com>
2022-12-15 02:09:36 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@ void print_alphabet_x10(void)
 	char i;
 	int a;
 
-	while(a <= 10)
+	while (a <= 10)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			{
 				_putchar(i);
 			}
+		a++;
 	}
 	_putchar('\n');
 }
