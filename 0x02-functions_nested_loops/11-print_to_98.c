7876f2bd8dd2726cb919d74f413a534577e399a1
Alfonse <jumaalfonse1@gmail.com>
2022-12-16 06:56:05 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		if (n > 98)
-			printf("%d, ", n--);
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 	else
 	{
 		if(n < 98)
-			printf("%d, ", n++);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 }
