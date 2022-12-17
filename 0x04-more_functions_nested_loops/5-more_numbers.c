d745acefe0172bfbc09fd89994e1a37f5f91292f
Alfonse <jumaalfonse1@gmail.com>
2022-12-17 02:08:59 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
+	for (b = 0; b <= 10; a++)
 	{
-		for (b = 1; b <= 14; b++)
+		for (a = 1; a <= 14; a++)
 		{
-			for (b >= 10)
+			for (a >= 10)
 			{
-				_putchar((b / 10) + '0');
+				_putchar((a / 10) + '0');
 			}
-		_putchar(b % 10) + '0');
+		_putchar(a % 10) + '0');
 		}
 	}
 	_putchar('\n');
