e02ccbcf30dcc9040f158c29ac9ad72e6c620aaf
Alfonse <jumaalfonse1@gmail.com>
2022-12-17 03:39:34 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print numbers 1 to 100
+ * 3 times prints fizz in place of the number
+ * 5 time prints buzz in place of a number
+ * 3 and 5 prints fuzzbuzz in plave of a number
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int a;
+	char Fizz, Buzz, FizzBuzz;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a == 100)
+			printf("%s", Buzz);
+		else if ((a % 3 == 0) && (i % 5 == 0))
+			printf("%s ", FuzzBuzz);
+		else if (a % 3 == 0)
+			printf("%s ", Fuzz);
+		else if (a % 5 == 0)
+			printf("%s ", Buzz);
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
