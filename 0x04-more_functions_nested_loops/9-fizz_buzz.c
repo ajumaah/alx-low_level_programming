5ac9933095b30069a3084dc69fcedc51c6562f98
Alfonse <jumaalfonse1@gmail.com>
2022-12-17 03:54:40 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,7 +24,7 @@ int main(void)
 			printf("%s ", f);
 		else if (a % 5 == 0)
 			printf("%s ", b);
-		else if ((a % 3 == 0) && (i % 5 == 0))
+		else if ((a % 3 == 0) && (a % 5 == 0))
 			printf("%s ", fb);
 		else
 			printf("%d ", a);
