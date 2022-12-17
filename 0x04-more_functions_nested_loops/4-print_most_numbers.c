907531152f308be7324caa4fafc807116a7dc063
Alfonse <jumaalfonse1@gmail.com>
2022-12-16 23:55:00 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_most_numbers - print 0 to 9 followed by a new line
- *
  * Return: numbers 0 to 9 excluding 2 and 4
  */
+
 void print_most_numbers(void)
 {
 	int k;
-	
-	for (k = 0; k < 10; k++)
+
+	for (k = 0; k <= 9; k++)
 	{
 		if ((k == 2) || (k == 4))
 			countinue;
