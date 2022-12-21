8461c8ab574fdde802fb2ecf9da8dd2e17e8893d
Alfonse <jumaalfonse1@gmail.com>
2022-12-21 04:22:22 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,13 @@ void print_rev(char *s)
 	i = 0;
 
 	while (s[i] != '\0')
-		i++;
 	{
-		for (i = -1; i >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
-		_putchar('\n');
+		i++;
 	}
+	for (i = -1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
 
