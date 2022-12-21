239cc9299afdadf0936e142ef61fa4848fe23364
Alfonse <jumaalfonse1@gmail.com>
2022-12-21 04:12:30 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse followed with a new line
+ * @s: string to be printed in reverse
+ *
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	{
+		for (i = -1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
+
