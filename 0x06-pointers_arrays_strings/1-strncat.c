fbf9547c882a2c1af4d954b489835bc052dff064
Alfonse <jumaalfonse1@gmail.com>
2022-12-23 06:12:00 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,16 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dlen = 0, k = 0;
 
-	while ((dest + dlen) != '\0')
+	while (dest[dlen++])
 	{
-		dlen++;
-	}
-	while ((src + k) != '\0' && dlen < 97 && k < n)
-	{
-		dest + dlen = src + k;
-		dlen++;
 		k++;
 	}
-	dest + dlen = '\0';
+	for (deln = 0; src[dlen] && dlen < n; dlen++)
+	{
+		dest[k++] = src[dlen];
+	}
 	return (dest);
 }
