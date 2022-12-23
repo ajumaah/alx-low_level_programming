61b75190d59565934ee3bdba73004ea5f85a051c
Alfonse <jumaalfonse1@gmail.com>
2022-12-23 03:03:05 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _stracat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: void
+ */
+char *_strcat(int n; char *dest, char *src)
+{
+	int len = 0, a = 0;
+
+	while (dest[len])
+	{
+		len++;
+	}
+	while (a < n && src[a])
+	{
+		dest[len] = src[a];
+		len++;
+		a++;
+	}
+	dest[len + n + 1] = '\0';
+	return (dest);
+}
