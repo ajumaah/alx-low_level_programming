bde36e7588af7d9f3bcb4360d9014d3b2b0d268e
Alfonse <jumaalfonse1@gmail.com>
2022-12-24 18:12:15 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
