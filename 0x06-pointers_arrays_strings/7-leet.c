d6d8aa63036b1272a3ffca1698f1ba1dbaf31e1e
Alfonse <jumaalfonse1@gmail.com>
2022-12-24 16:27:56 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @n: input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *leet(char *n)
+{
+	int j, k;
+	int letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replaced_by[] = {'4', '3', '0', '7', '1'};
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			if (n[j] == letters[k])
+			{
+				n[j] = replaced_by[k /2];
+				k = 10;
+			}
+		}
+	}
+	return (n);
+}
