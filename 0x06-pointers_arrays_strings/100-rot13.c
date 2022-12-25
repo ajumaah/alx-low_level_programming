52a911c7d5f0411938e6a01e68347df2d68a0c73
Alfonse <jumaalfonse1@gmail.com>
2022-12-24 18:02:37 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char *rot13(char *n)
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for ( j = 0; n[j] != '\0'; j++)
+	for (j = 0; n[j] != '\0'; j++)
 	{
 		for (k = 0; k < 52; k++)
 		{
