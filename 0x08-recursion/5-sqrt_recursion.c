5c8da2c71011fe2563b78b2e84fe487b492e173e
Alfonse <jumaalfonse1@gmail.com>
2023-01-09 03:29:13 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,10 +8,43 @@
  * Return: Always 0 (Success)
  */
 
+int sqr_odd(int a, int n);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n <= 1 && n >= 0)
 		return (n);
+	if (!(n % 2))
+	{
+		if (n / 2 == 2 || n == 2)
+			return (2);
+		sqr = _sqrt_recursion(n / 2);
+		if (n / sqr == sqr)
+			return (sqr);
+		else if (sqr > 1)
+			return (sqr * 2);
+		return (-1);
+		else
+		{
+			return (sqr_odd(n, n - 2));
+		}
+	}
+}
+
+/**
+ * sqr_odd - check for prime  odd numbers
+ * @a: the prime or odd number to be checked
+ * @n: the number to find its square root
+ *
+ * Return: square root
+ */
+int sqr_odd(int a, int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (a == n * n)
+		return (n);
+	else
+		return (sqr_odd(a, n - 2));
 }
