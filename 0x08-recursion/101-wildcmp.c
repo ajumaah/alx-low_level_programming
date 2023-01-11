26cb1b40c03ad3f0ca1be8383d7d7879de82e95b
Alfonse <jumaalfonse1@gmail.com>
2023-01-11 03:43:27 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,12 +14,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 != '\0' && *s2 != '\0')
 		return (0);
 	else if (*s1 != *s2)
-		return (wildcmp(s1 - 1, s2 - 1));
+		return (wildcmp(*s1 - 1, *s2 - 1));
 	else if (*s2 != '*')
 	{
 		if (*(s2 - 1) != '*')
-			return (wildcmp(s1, s2 - 1));
-		if (wildcmp(s1 - 1, s2 || wildcmp(s1, s2 - 1)))
+			return (wildcmp(*s1, *s2 - 1));
+		if (wildcmp(*s1 - 1, *s2 || wildcmp(*s1, *s2 - 1)))
 			return (0);
 	}
 	return (1);
