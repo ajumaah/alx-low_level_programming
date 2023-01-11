b0beeec0a295fcc5a120435301a248b6b8a3eda9
Alfonse <jumaalfonse1@gmail.com>
2023-01-11 02:57:08 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palidrome - returns one if a string is palidrome else
+ * @s: the string
+ *
+ * Return: 1 if string is palidrome and 0 if not
+ */
+
+int is_palidrome(char *s)
+{
+	int a = 0;
+
+	if (*s == a)
+		return (0);
+	else
+		return (1);
+}
