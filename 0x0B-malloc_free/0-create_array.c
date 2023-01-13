b4afd49539d973d6758d4a83215d03e1d9ef3023
Alfonse <jumaalfonse1@gmail.com>
2023-01-13 01:48:09 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,9 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and initializes
  * @c: char in the array
+ * @size: array size
  *
  * Return: void
  */
