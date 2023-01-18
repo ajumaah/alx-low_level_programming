558ffdc39df263bc3089018b2c011c251c8f836c
Alfonse <jumaalfonse1@gmail.com>
2023-01-18 04:19:02 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,17 +7,24 @@
  * @argv: array pointing to arguments
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int j, k;
+	int j, add = 0;
 
-	if (argc == 3)
-	{
-		j = atoi(argv[1]);
-		k = atoi(argv[2]);
-		printf("%d\n", j *k);
+	if (argc < 1)
 		return (0);
+
+	for (j = 1; j < argc; j++)
+	{
+		if (!atoi(argv[j]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		add += atoi(argv[j]);
 	}
-	printf("Error\n");
-	return (1);
+	printf("%d\n", add);
+
+	return (0);
 }
