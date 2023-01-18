91f751d8cfbb85c2b5ed51d069f42a96fc0663a9
Alfonse <jumaalfonse1@gmail.com>
2023-01-18 02:21:15 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,13 +6,14 @@
  * @b: input amount
  * Return: pointer to new mem
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *d;
-
-	d = malloc(b);
+	void *d = malloc(b);
 
 	if (d == NULL)
+	{
 		exit(98);
+	}
 	return (d);
 }
