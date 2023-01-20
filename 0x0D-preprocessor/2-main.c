c092d829469e2f41999dcdcc6ce6d503ad33f84e
Alfonse <jumaalfonse1@gmail.com>
2023-01-20 07:41:32 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of a file
+ *
+ * Return: Always 0 (Success)
+ */
+
+in main(int argc, char arga[])
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
