77b75efe54756cf336bfbf43878c6787d540e36a
Alfonse <jumaalfonse1@gmail.com>
2023-01-21 00:33:02 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,8 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int k;
 
 	if (action == NULL || array == NULL)
-		exit(0); //terminates when action and array are null
-
+		exit(0);
 	for (k = 0; k < size; k++)
 		action(array[k]);
 }
