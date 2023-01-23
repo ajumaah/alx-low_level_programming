924692d56a462706a73d4ab29483941ff9e4bb75
Alfonse <jumaalfonse1@gmail.com>
2023-01-23 08:28:08 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,8 +16,9 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(list, n);
-	for (; i < n; i++)
+	while (i < n)
 	{
+		i++;
 		sum += va_arg(list, int);
 	}
 	va_end(list);
