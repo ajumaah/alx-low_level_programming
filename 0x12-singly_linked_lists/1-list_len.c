bdfcf1bc4172166d5ae18cbc8044c54986195100
Alfonse <jumaalfonse1@gmail.com>
2023-02-02 04:05:10 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,14 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
+ * list_len - Find number of elements in
+ * the linked list_t list.
  * @h: The linked list_t list.
  *
  * Return: The number of elements in h.
  */
+
+
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
